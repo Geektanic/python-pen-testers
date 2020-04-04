7c847ac5547d6c079456e21483ec1fd1cc9ecb72
Geektanic <doug.rhodes@gmail.com>
2020-04-03 22:56:48 -0400

    Synch.

---
+++ tcp-server-client/server.py
@@ -0,0 +1,20 @@
+# server.py
+
+import socket
+
+host = socket.gethostname()
+port = 9337
+
+sock_ = socket.socket(socket.AF_INET,socket.SOCK_STREAM) # TCP communication, SOCK_DGRAM for UDP
+sock_.bind((host,port))
+sock_.listen(1)
+
+print("\nServer started...\n")
+
+conn,addr = sock_.accept()
+
+print("Connection established with: " + str(addr))
+
+message = "\nThank you for connecting " + str(addr)
+conn.send(message.encode("ascii"))
+conn.close()
